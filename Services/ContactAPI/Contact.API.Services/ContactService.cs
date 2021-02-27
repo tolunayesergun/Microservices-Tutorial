202d21b0f588b3f077416f032451c3948c550aed
tolunayesergun <tolunay50@gmail.com>
2021-02-27 13:23:22 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API.Services/ContactService.cs
@@ -0,0 +1,21 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact.API.Services
+{
+    public class ContactService : IContactService
+    {
+        public ContactDTO GetContactById(int Id)
+        {
+            return new ContactDTO() //db olabilirdi
+            {
+                Id = Id,
+                FirstName = "Tolunay",
+                LastName = "Esergün"
+            };
+        }
+    }
+}
---
+++ Services/ResAPI/Reservation.API.Services/ReservationService.cs
@@ -0,0 +1,24 @@
+﻿using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reservation.API.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationDTO GetResByBkgNumber(int BkgNumber)
+        {
+            return new ReservationDTO()
+            {
+                Id = (new Random()).Next(100),
+                Amount = (new Random()).Next(1000),
+                BkgDate = DateTime.Now,
+                CheckInDate = DateTime.Now.Date.AddDays(30),
+                CheckOutDate = DateTime.Now.Date.AddDays(37),
+                BkgNumber = BkgNumber
+            };
+        }
+    }
+}
